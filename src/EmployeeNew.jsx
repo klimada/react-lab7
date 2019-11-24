12cdaaa96a51b23fb0650a971b0528100ee8bb64
klimada <56273947+klimada@users.noreply.github.com>
2019-11-24 16:29:31 +0100

    Lab7task3

--- src/EmployeeNew.jsx
+++ src/EmployeeNew.jsx
@@ -6,14 +6,20 @@ class EmployeeNew extends React.Component {
       this.state = {
         age: 17,
         isActive: true,
+        email:"",
+        phone:"",
+        emailisValid:true,
+        phoneisValid:true,
       };
       this.somethingChanged = this.somethingChanged.bind(this);
       this.isActiveChanged = this.isActiveChanged.bind(this);
+      this.validation = this.validation.bind(this);
     }
 
     somethingChanged(event) {
         this.setState({
             [event.target.name]: event.target.value, 
+            phoneisValid:true,emailisValid:true,
         });
     }
 
@@ -22,20 +28,36 @@ class EmployeeNew extends React.Component {
         this.setState({isActive});
     }
 
+    validation()
+    {
+      if(this.state.age >= 18)
+      {
+        const emailTest = /^[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+        emailTest.test(this.state.email) ? this.setState({emailisValid: true}) : this.setState({emailisValid: false});
+       }
+      else
+      {
+        const phoneTest = /[0-9]{9}/;
+        phoneTest.test(this.state.phone) ? this.setState({phoneisValid: true}) : this.setState({phoneisValid: false});
+       }
+    }
+
     render() { 
-        const { age } = this.state;
+        const { age,isActive,email,phone,emailisValid,phoneisValid} = this.state;
         return (
             <div>
-            <form style={{ margin: "15px 0 0 50px" }} > 
+            <form style={{ margin: "15px 0 0 50px" }}  onSubmit={event => {event.preventDefault(); this.validation()}}> 
                 <label style={{ margin: "15px 0 0 50px" }}> Age <input type="number" value={age} name="age" onChange={this.somethingChanged}/></label>
                 <br></br>
                 {(age >= 18) ? <label style={{ margin: "15px 0 0 50px"}}> Name: <input type="text"  /> </label>
                              : <label style={{ margin: "15px 0 0 50px" }}>Parent Name:<input type="text"  /></label>}
                 <br></br>
-                {(age >= 18) ? <label style={{ margin: "15px 0 0 50px" }}> Email: <input type="text" /></label>
-                             : <label style={{ margin: "15px 0 0 50px" }}> Parent Phone No: <input type="text" /></label>}
+                {(age >= 18) ? <div><label style={{ margin: "15px 0 0 50px" }}> Email: <input type="text" value={email} name="email" onChange={this.somethingChanged} /></label>
+                              {!emailisValid ? <label style={{fontSize:"10px",color:"red"}}>Incorrect email address</label> : null}</div>
+                             : <label style={{ margin: "15px 0 0 50px" }}> Parent Phone No: <input type="text" value={phone} name="phone" onChange={this.somethingChanged}/></label>}
+                             {!phoneisValid ? <label style={{fontSize:"10px",color:"red"}}>Incorrect phone address</label> : null}
                 <br></br>
-                <input type="submit" value="Submit" style={{ margin: "15px 0 0 250px" }}/>
+                <input type="submit" value="Submit" style={{ margin: "15px 0 0 250px"}} />
               </form>
         </div>          
         );
